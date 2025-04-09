146162d14a9e8c66276e45bd859cadfd44cf4d3c
Parker Downing <parkeraydowning@gmail.com>
2025-04-09 08:34:55 -0600

    changes to use forwardRef; Adds useImperativeHandle

--- section8/01-starting-project/src/components/ResultModal.jsx
+++ section8/01-starting-project/src/components/ResultModal.jsx
@@ -1,6 +1,19 @@
-export default function ResultModal({ref, result, targetTime}) {
+import { forwardRef, useImperativeHandle, useRef } from "react"
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  });
+
+
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -9,4 +22,6 @@ export default function ResultModal({ref, result, targetTime}) {
       </form>
     </dialog>
   )
-}
+})
+
+export default ResultModal;
