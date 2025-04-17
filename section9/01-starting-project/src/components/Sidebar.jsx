69f8f509272f6799b20991410a4c585208e5450b
Parker Downing <parkeraydowning@gmail.com>
2025-04-17 08:10:28 -0600

    Adds Sidebar component

---
+++ section9/01-starting-project/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+
+export default function Sidebar() {
+  return (
+    <>
+      <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+        <h1 className="text-3xl font-bold text-stone-600 mb-2">Your Projects</h1>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add Project</button>
+      </aside>
+      
+      
+    </>
+
+  )
+}
