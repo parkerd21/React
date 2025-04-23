90ab782d560b2697445ac79ddff1312e54229cc7
Parker Downing <parkeraydowning@gmail.com>
2025-04-23 08:18:35 -0600

    Updates Input html

--- section9/01-starting-project/src/components/Input.jsx
+++ section9/01-starting-project/src/components/Input.jsx
@@ -1,9 +1,12 @@
-export default function Input({title, type="text"}) {
+export default function Input({label, textarea = false, ...props}) {
+  const sharedClassName = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
   return (
-    <>
-      <label className="text-sm font-bold uppercase text-stone-500">{title}</label>
-      <input type={type} className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"/>
-    </>
+    <p>
+      <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+      {textarea ? <textarea {...props} className={sharedClassName}/>
+        : <input {...props} className={sharedClassName}/>}
+      
+    </p>
   )
 
 }
