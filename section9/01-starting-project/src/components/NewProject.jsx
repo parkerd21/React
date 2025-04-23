f48c70e46bebe014e3c90ac7563fe4e9645832ce
Parker Downing <parkeraydowning@gmail.com>
2025-04-23 08:19:51 -0600

    Updates html

--- section9/01-starting-project/src/components/NewProject.jsx
+++ section9/01-starting-project/src/components/NewProject.jsx
@@ -3,17 +3,22 @@ import Input from "./Input";
 export default function NewProject({onSave}) {
   
   return (
-    <form className="mt-4 text-right" >
+   
       <div>
-        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+        <menu>
+          <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+          <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+        </menu>
+        <div>
+          <Input label="Title" />
+          <Input label="Description" textarea={true}/>
+          <Input label="Due Date"/>
+        </div>
+        
       </div>
 
-      <Input title="TITLE" />
-      <Input title="DESCRIPTION"/>
-      <Input title="DUE DATE" type="date" />
+
       
 
-    </form>
   )
 }
