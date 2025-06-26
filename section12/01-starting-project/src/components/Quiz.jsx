ffbc3cc5f6bde2b8000d69a85a19ed44e0597f1a
Parker Downing <parkeraydowning@gmail.com>
2025-06-26 09:10:54 -0600

    Adds summary page

--- section12/01-starting-project/src/components/Quiz.jsx
+++ section12/01-starting-project/src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from 'react'
 import QUESTIONS from '../questions.js'
 import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
-import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([])
@@ -19,10 +19,9 @@ export default function Quiz() {
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
   if (quizIsComplete) {
-    return <div id="summary">
-      <img src={quizCompleteImg} alt="Trophy icon" />
-      <h2>Quiz Completed!</h2>
-    </div>
+    return <Summary
+      userAnswers={userAnswers}
+    />
   }
 
   return (
