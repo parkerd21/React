02b12961498bb1d7e11e078168e517b7fd55cb98
Parker Downing <parkeraydowning@gmail.com>
2025-07-10 09:03:04 -0600

    Adds loading text and functionality

--- section15/01-starting-project/src/components/AvailablePlaces.jsx
+++ section15/01-starting-project/src/components/AvailablePlaces.jsx
@@ -3,14 +3,17 @@ import { useState, useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places')
       const resData = await response.json()
       setAvailablePlaces(resData.places)
+      setIsFetching(false);
     }
-    
+
     fetchPlaces()
   }, [])
 
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
