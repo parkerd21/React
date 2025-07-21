f84510cab6ce0dd84d76db1915ed9e0e4715a404
Parker Downing <parkeraydowning@gmail.com>
2025-07-21 09:40:41 -0600

    Adds input validation

--- section17/01-starting-project/src/components/StateLogin.jsx
+++ section17/01-starting-project/src/components/StateLogin.jsx
@@ -9,8 +9,16 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
+
     console.log({ enteredValues });
   }
 
@@ -18,6 +26,17 @@ export default function Login() {
     setEnteredValues((prevState) => ({
       ...prevState,
       [identifier]: value,
+    }));
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false
+    }))
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -32,9 +51,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
