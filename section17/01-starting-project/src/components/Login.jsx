35e8bdbf3f14f231060c70a393b9a40726fcb690
Parker Downing <parkeraydowning@gmail.com>
2025-07-22 08:33:01 -0600

    Adds submit validation for Login component that uses refs

--- section17/01-starting-project/src/components/Login.jsx
+++ section17/01-starting-project/src/components/Login.jsx
@@ -1,18 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  const email = useRef()
-  const password = useRef()
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const enteredEmail = email.current.value
-    const enteredPassword = password.current.value
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
 
-    console.log({enteredEmail, enteredPassword})
+    const emailIsValid = enteredEmail.includes("@");
 
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("Sending http request... ");
   }
 
   return (
@@ -22,22 +31,15 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            ref={email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            ref={password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
