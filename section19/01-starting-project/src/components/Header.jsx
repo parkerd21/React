8b18f68e7c4412e08aead3bccaa509982a2f5531
Parker Downing <parkeraydowning@gmail.com>
2025-08-04 09:52:29 -0600

    Adds Header component

--- section19/01-starting-project/src/components/Header.jsx
+++ section19/01-starting-project/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import logoImg from "../assets/logo.jpg";
 
-export function Header() {
+export default function Header() {
   return (
-    <header>
-      <img src={logoImg} alt="Food and beverage" />
-      <h1>Order Up</h1>
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="Food and beverage" />
+        <h1>Order Up</h1>
+      </div>
+      <nav>
+        <button>Card (0)</button>
+      </nav>
     </header>
   );
 }
