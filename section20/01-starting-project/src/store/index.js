203faa81ae204ca4351c9c7bd30b37853b56ec79
Parker Downing <parkeraydowning@gmail.com>
2025-08-29 09:00:07 -0600

    315. Adds toggle dispatch

--- section20/01-starting-project/src/store/index.js
+++ section20/01-starting-project/src/store/index.js
@@ -1,27 +1,39 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
 
-  if (action.type === 'increase') {
+  if (action.type === "increase") {
     return {
+      ...state,
       counter: state.counter + action.amount,
-    }
+    };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
     };
   }
+
+  if (action.type === 'toggle') {
+    return {
+      ...state,
+      showCounter: !state.showCounter
+    }
+  }
+  
   return state;
 };
 
 const store = createStore(counterReducer);
 
 export default store;
-
