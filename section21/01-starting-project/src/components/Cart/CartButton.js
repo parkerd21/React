1b8d8f0551dd28dcec2e73c84ae66a324d582689
Parker Downing <parkeraydowning@gmail.com>
2025-09-03 18:28:55 -0600

    Adds initial redux and toggleCart action

--- section21/01-starting-project/src/components/Cart/CartButton.js
+++ section21/01-starting-project/src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch()
+  function handleClick() {
+    dispatch(uiActions.toggleCart());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleClick}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
---
+++ section21/01-starting-project/src/store/ui-slice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialUiState = { showCart: true };
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: initialUiState,
+  reducers: {
+    toggleCart(state) {
+      state.showCart = !state.showCart;
+    },
+  },
+});
+export const uiActions = uiSlice.actions;
+export default uiSlice.reducer;
